13af91969f6594e1e69a35cdb74595e6539011b0
setor <marik123456789110011@gmail.com>
2026-06-15 20:16:17 +0300

    feet: some fixes

--- src/main/java/com/innowise/orderservice/client/UserServiceClient.java
+++ src/main/java/com/innowise/orderservice/client/UserServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "userservice", url = "${spring.service.user-service.url}")
+@FeignClient(name = "userservice", url = "${app.service.user-service.url}")
 public interface UserServiceClient {
 
     @GetMapping("/api/users/{id}")
